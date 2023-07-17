1bd0e0efdb1a74ab28285119b7ca2125ab5fca53
PavithraParthi <139786468+PavithraParthi@users.noreply.github.com>
2023-07-17 20:33:08 +0530

    Add files via upload
    Day1

---
+++ myassignments/Firefox.java
@@ -0,0 +1,24 @@
+package myassignments;
+/*17 July Assignments on datatypes
+   1) version = 100.2  (use dataType float) 
+   2) name = "firefox" (use dataType String)
+   3) visible = true (use dataType boolean)
+   4) year = 1998 (use dataType int)
+   5) logo = 'm' (Use datatype char) */
+
+public class Firefox {
+public static void main(String[] args) {
+	float version=100.2f;
+	String name="firefox";
+	boolean visible=true;
+	int year=1998;
+	char logo='m';
+	System.out.println("Firefox version:"+ version);
+	System.out.println("Browser Name:"+ name);
+	System.out.println("Browser visible:"+ visible);
+	System.out.println("Browser stared:"+ year);
+	System.out.println("Browser Logo:"+ logo);
+
+	}
+
+}
---
+++ week1/day1/CarDatatype.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class CarDatatype {
+	public static void main(String[] args) {
+
+	//datatype variable=value
+       
+		int price=100000;        
+        String brandName="Tata"; //camalCase        
+        //String hold collection of characters
+        char logo='T';        
+        float fuel=12.45f;    
+        String fuelCapcity="12.45lt";
+        double modelVariant=456.2343565757;
+        boolean isNew=false;        
+        long phNo=673528208366722l;
+        
+        System.out.println("logo is : "+logo);        
+        System.out.println(logo);        
+        System.out.println("The brand name of the Car :"+brandName);
+        System.out.println("Price of the car:"+price);
+        System.out.println(fuel);
+        System.out.println("Fuel capacity of the car:"+fuelCapcity);
+        System.out.println(modelVariant +": "+isNew);
+        System.out.println("Phone number:"+phNo);
+	}
+
+}
